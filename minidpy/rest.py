d863d14d9b356ed4f665c6aeb0351f1563064d1a
omame <me@omame.xyz>
2023-08-30 00:30:57 +0200

    hell

--- minidpy/rest.py
+++ minidpy/rest.py
@@ -23,17 +23,17 @@ class REST:
                 "User-Agent"
             ] = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36"
 
-    async def _request(self, method: str, endpoint: str, data: any):
+    async def _request(self, method: str, endpoint: str, resp_data: any):
         resp = await self._session.request(
-            method, f"{_BASE_URL}/v{self._version}{endpoint}", json=data
+            method, f"{_BASE_URL}/v{self._version}{endpoint}", json=resp_data
         )
-        data = await resp.json()
-        if "retry_after" in data:
-            await asyncio.sleep(data["retry_after"])
+        resp_data = await resp.json()
+        if "retry_after" in resp_data:
+            await asyncio.sleep(resp_data["retry_after"])
             return await self._request(method, endpoint, data)
-        if "code" in data and "message" in data:
-            raise RESTError(data["code"], data["message"])
-        return data
+        if "code" in resp_data and "message" in resp_data:
+            raise RESTError(resp_data["code"], resp_data["message"])
+        return resp_data
 
     async def get(self, endpoint: str):
         return await self._request("GET", endpoint, None)
